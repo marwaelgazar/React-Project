ee48b7cbc1334856083de56173a4e09afbb04b85
marwaelgazar <marwaelgazar1@gmail.com>
2024-10-20 22:19:53 +0300

    change path

--- src/components/buttons/Login.jsx
+++ src/components/buttons/Login.jsx
@@ -43,24 +43,24 @@ const Login = () => {
                 Facebook
               </button>
               <form>
-                <div class="mb-3">
-                  <label for="email" class="form-label">
+                <div className="mb-3">
+                  <label htmlFor="email" class="form-label">
                     Email Address
                   </label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
                     id="email"
                     required
                   />
                 </div>
                 <div class="mb-3">
-                  <label for="password" class="form-label">
+                  <label htmlFor="password" className="form-label">
                     Password
                   </label>
                   <input
                     type="password"
-                    class="form-control"
+                    className="form-control"
                     id="password"
                     required
                   />
