c49b9a523006484ac7294aae1c846fa775f4c263
marwaelgazar <marwaelgazar1@gmail.com>
2024-10-21 03:06:30 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import Products from "./Products";
-
+import bgi from "../imgs/bgi.webp"
 export default function Home() {
   return (
     <>
       <div className="hero">
         <div className="card text-bg-dark border-0">
           <img
-            src="/public/assets/images/bgi.webp"
+            src={bgi}
             className="card-img"
             alt="background"
             height="550px"
